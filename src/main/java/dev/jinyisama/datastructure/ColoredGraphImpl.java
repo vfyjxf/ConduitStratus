f02bcac01a6c8b24e4c3736e3ee19fb99f991a65
Jinyi-X <jinyisama@foxmail.com>
2024-07-19 14:47:43 +0800

    Just upload ColoredGraphInterface

--- src/main/java/dev/jinyisama/datastructure/ColoredGraphImpl.java
+++ src/main/java/dev/jinyisama/datastructure/ColoredGraphImpl.java
@@ -1,2 +1,13 @@
-package dev.jinyisama.datastructure;public class ColoredGraphImpl {
+package dev.jinyisama.datastructure;
+
+import java.util.List;
+
+public class ColoredGraphImpl{
+    public class ImplOnCPU implements ColoredGraphInterface {
+        public List<Misc.worldCoordinate> usedColors() {return null;}
+        public boolean deleteNode(Misc.worldCoordinate coordinate) {return false;}
+        public boolean isConnectable(Misc.worldCoordinate coordinate) {return false;}
+        public boolean insertNode(Misc.worldCoordinate coordinate, Misc.NodeColor color) {return false;}
+        public boolean isConnected(Misc.worldCoordinate coordinate1, Misc.worldCoordinate coordinate2) {return false;}
+    }
 }
--- src/main/java/dev/jinyisama/datastructure/ColoredGraphInterface.java
+++ src/main/java/dev/jinyisama/datastructure/ColoredGraphInterface.java
@@ -1,2 +1,11 @@
-package dev.jinyisama.datastructure;public interface ColoredGraphInterface {
+package dev.jinyisama.datastructure;
+
+import java.util.List;
+
+public interface ColoredGraphInterface {
+    List<Misc.worldCoordinate> usedColors();
+    boolean deleteNode(Misc.worldCoordinate coordinate);
+    boolean isConnectable(Misc.worldCoordinate coordinate);
+    boolean insertNode(Misc.worldCoordinate coordinate, Misc.NodeColor color);
+    boolean isConnected(Misc.worldCoordinate coordinate1, Misc.worldCoordinate coordinate2);
 }
