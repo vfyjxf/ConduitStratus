2c1ee8a3b2299877536df2469daac78960f0573e
vfyjxf_ <2331007009@qq.com>
2024-11-28 12:34:23 +0800

    upload metadata files

--- src/main/java/dev/vfyjxf/conduitstratus/ConduitStratus.java
+++ src/main/java/dev/vfyjxf/conduitstratus/ConduitStratus.java
@@ -1,19 +1,33 @@
 package dev.vfyjxf.conduitstratus;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import dev.vfyjxf.conduitstratus.api.StratusRegisterEvent;
 import dev.vfyjxf.conduitstratus.config.Config;
 import dev.vfyjxf.conduitstratus.init.StratusRegistryImpl;
 import dev.vfyjxf.conduitstratus.init.values.ModValues;
 import dev.vfyjxf.conduitstratus.utils.tick.TickDispatcher;
+import net.minecraft.client.KeyMapping;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.ModLoader;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.client.settings.KeyConflictContext;
+import net.neoforged.neoforge.client.settings.KeyModifier;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class ConduitStratus {
     public static final Logger logger = LoggerFactory.getLogger("ConduitStratus");
+
+    public static final KeyMapping openTestScreen = new KeyMapping(
+            "Open Test Screen",
+            KeyConflictContext.GUI,
+            KeyModifier.CONTROL,
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_R,
+            "debug"
+    );
 
     public ConduitStratus(IEventBus modBus, ModContainer modContainer) {
         Config.register(modContainer);
--- src/main/java/dev/vfyjxf/conduitstratus/ui/graph/TestCodeGraphScreen.java
+++ src/main/java/dev/vfyjxf/conduitstratus/ui/graph/TestCodeGraphScreen.java
@@ -1,33 +1,21 @@
 package dev.vfyjxf.conduitstratus.ui.graph;
 
-import com.mojang.blaze3d.platform.InputConstants;
 import dev.vfyjxf.cloudlib.api.ui.event.WidgetEvent;
 import dev.vfyjxf.cloudlib.api.ui.widgets.Widget;
 import dev.vfyjxf.cloudlib.api.ui.widgets.WidgetGroup;
 import dev.vfyjxf.cloudlib.ui.ModularScreen;
 import dev.vfyjxf.conduitstratus.Constants;
-import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.InputEvent;
-import net.neoforged.neoforge.client.settings.KeyConflictContext;
-import net.neoforged.neoforge.client.settings.KeyModifier;
 import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.list.MutableList;
-import org.lwjgl.glfw.GLFW;
+
+import static dev.vfyjxf.conduitstratus.ConduitStratus.openTestScreen;
 
 @EventBusSubscriber(modid = Constants.MOD_ID)
 public class TestCodeGraphScreen extends ModularScreen {
-
-    private static final KeyMapping openTestScreen = new KeyMapping(
-            "Open Test Screen",
-            KeyConflictContext.GUI,
-            KeyModifier.ALT,
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_R,
-            "debug"
-    );
 
     private static final MutableList<String> testLabels = Lists.mutable.of(
             "add", "sub", "mul", "div", "mod", "and", "or",
@@ -56,7 +44,7 @@ public class TestCodeGraphScreen extends ModularScreen {
             });
             labelWidget.setPos(0, i * 20);
             labelWidget.setSize(methodTab.getWidth(), 20);
-            methodTab.asChild(methodTab);
+            labelWidget.asChild(methodTab);
         }
         methodTab.asChild(mainGroup);
         var codeGraph = new WidgetGroup<>();
